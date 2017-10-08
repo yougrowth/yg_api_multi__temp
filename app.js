cada94a15e6423104815289e89c6083aed199bbb
Lucas Tavares <lucastavaresdarocha@gmail.com>
2017-10-08 13:31:37 -0300

    update

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ const app = express()
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  //res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
   next()
 })
