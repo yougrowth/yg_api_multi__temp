7e18b32c1ed5609b72ab6fe5fc70b18f854e760f
Lucas Tavares <lucastavaresdarocha@gmail.com>
2017-10-08 11:08:57 -0300

    allow origin

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ if (process.env.NODE_ENV !== 'prod') {
 
 const app = express()
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  //res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next()
+})
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/usuario', usuarioRoute(Router()))
